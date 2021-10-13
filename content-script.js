bcd136e2d8910029bb4e022b67cfebea30f5ab98
Victor Zheng <vzheng@gmail.com>
2021-10-12 21:46:49 -0400

    properly styled the copy and close button, close button functionality done

--- content-script.js
+++ content-script.js
@@ -1,26 +1,45 @@
-const grabSelected = (e) => {
+let prevSelected = "";
+
+const copyText = () => {
   const selected = window.getSelection().toString();
-  // if there is no selected text, dont add anything to the doc
   if (selected === "") {
     return;
   }
+};
+
+const closeTooltip = () => {
+  $("#copy-tooltip").remove();
+};
+
+const createTooltip = (e) => {
+  const selected = window.getSelection().toString();
+  // if there is no selected text, dont add anything to the doc
+  if (selected === "" || prevSelected === selected) {
+    return;
+  }
+  prevSelected = selected;
   const xAxis = e.pageX;
   const yAxis = e.pageY;
-  console.log(`x axis is ${xAxis}`);
-  console.log(`y axis is ${yAxis}`);
-  const copyButton = $('<button id="copy-button">Copy<button/>');
-  const closeButton = $('<button id="close-button">Close<button/>');
-  const copyTooltip = $("<div id=copy-tooltip>").css({
-    left: `${xAxis} px`,
-    top: `${yAxis} px`,
+  const copyButton = $("<button/>", {
+    id: "copy-button",
+    text: "Copy",
+    click: copyText,
   });
+  const closeButton = $("<button/>", {
+    id: "close-button",
+    text: "Close",
+    click: closeTooltip,
+  });
+  const copyTooltip = $("<div>").css({
+    position: "absolute",
+    left: `${xAxis}px`,
+    top: `${yAxis}px`,
+  });
+  copyTooltip.attr("id", "copy-tooltip");
   copyTooltip.append(copyButton);
   copyTooltip.append(closeButton);
-  copyTooltip.appendTo("body");
-  // copyTooltip.appendTo(document.body);
-  // const copyTooltip = document.createElement("div");
-
-  console.log(`selected text is ${selected}`);
+  $("body").append(copyTooltip);
   return;
 };
-document.addEventListener("mouseup", grabSelected);
+document.addEventListener("mouseup", createTooltip);
+document.addEventListener("mousedown", closeTooltip);
